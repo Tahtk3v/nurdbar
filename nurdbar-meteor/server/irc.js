aa548a5bcc04541e0980c4c15eca135e87e641c5
nooitaf <nooitaf@gmail.com>
2015-08-30 19:54:37 +0200

    removme

--- nurdbar-meteor/server/irc.js
+++ nurdbar-meteor/server/irc.js
@@ -1,4 +1,4 @@
-// code to run on server at startup
+  z// code to run on server at startup
 var irc_params = {
   server: Meteor.settings.server,
   port: 6667,
@@ -93,9 +93,7 @@ Meteor.startup(function() {
           }
 
           if (args[0] === '~aliases') {
-            log('let me lok up your alias...')
             Meteor.call('userAliases', user);
-            log('did i show it?')
           }
 
           if (args[0] === '~aliasadd') {
