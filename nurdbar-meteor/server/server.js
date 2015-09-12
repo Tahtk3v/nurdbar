fdf1d31337b763794f2e65a570d851fcdb03a800
zarya <zarya@gigafreak.net>
2015-09-12 12:23:02 +0200

    Update server.js
    Changed the shame speak

--- nurdbar-meteor/server/server.js
+++ nurdbar-meteor/server/server.js
@@ -394,7 +394,7 @@ Meteor.methods({
       {
         log(s.sprintf("Sold %s x %s to %s for %.2f euro.",amount, product.name, user.name, product.price * amount))
         if (user.cash < 0.0) {
-          log('!speak ' + user.name + ' has no moneyz!');
+          log('!speak ' + user.name + ' shame on you.');
         }
       }
       if (!nobon) {
