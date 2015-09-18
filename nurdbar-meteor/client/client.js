12ad377611708f767ef44490c307df5ea8a0c834
nooitaf <nooitaf@gmail.com>
2015-09-18 21:15:45 +0200

    simple listing

--- nurdbar-meteor/client/client.js
+++ nurdbar-meteor/client/client.js
@@ -1,4 +1,25 @@
+Products = new Mongo.Collection('products');
+Barusers = new Mongo.Collection('barusers');
+Book = new Mongo.Collection('book');
+BarLog = new Mongo.Collection('barlog');
+
 
 Router.route('/', function () {
   this.render('page');
 });
+
+
+Template.page.helpers({
+  products: function(){
+    return Products.find({},{sort:{name:1}})
+  },
+  barusers: function(){
+    return Barusers.find({},{sort:{name:1}})
+  },
+  book: function(){
+    return Book.find()
+  },
+  barlog: function(){
+    return Barlog.find()
+  }
+})
