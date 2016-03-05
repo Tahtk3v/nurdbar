9d2601ef6c602ea89c9dc6fb41c279d3ee2233be
nooitaf <nooitaf@gmail.com>
2016-03-05 19:48:20 +0100

    left padded in search AND list

--- nurdbar-meteor/server/server.js
+++ nurdbar-meteor/server/server.js
@@ -257,7 +257,7 @@ Meteor.methods({
       var row = '';
       row += s.lpad(item.stock, 4, " ");
       row += ' | ';
-      row += s.rpad(s.sprintf('%.2f',item.price), 6, " ");
+      row += s.lpad(s.sprintf('%.2f',item.price), 6, " ");
       row += ' | ';
       row += s.rpad(item.name, 15, " ");
 
