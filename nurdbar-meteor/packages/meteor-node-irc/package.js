a7104c25344941664014b83be47d2599135ff9e6
nooitaf <nooitaf@gmail.com>
2022-08-01 19:28:32 +0200

    bar: irc package update (2015 -> 2022)

--- nurdbar-meteor/packages/meteor-node-irc/package.js
+++ nurdbar-meteor/packages/meteor-node-irc/package.js
@@ -8,9 +8,9 @@ Npm.depends({
     "irc": "0.3.9"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@1.0');
-  api.add_files('irc.js', 'server');
+  api.addFiles('irc.js', 'server');
   if(api.export) api.export('IRC', 'server');
 });
  
