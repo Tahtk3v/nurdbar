fca6269b35c4f70e43b6621fc7b63a2c514a9adc
Kevin B <42434740+Tahtk3v@users.noreply.github.com>
2022-11-10 08:13:37 +0100

    Update main.js
    Commented out all scanner functionality

--- nurdbar-cli/main.js
+++ nurdbar-cli/main.js
@@ -621,23 +621,23 @@ screen.render();
 // ========================
 // SERIAL 
 // ========================
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort(config.serialport, {
-    baudrate: 9600
-  },
-  false, // this is the openImmediately flag [default is true]
-  function(error) {
-    console.log(error);
-  }
-);
+// var SerialPort = require("serialport").SerialPort
+// var serialPort = new SerialPort(config.serialport, {
+   // baudrate: 9600
+ // },
+  //false, // this is the openImmediately flag [default is true]
+  //function(error) {
+    //console.log(error);
+ // }
+//);
 
-serialPort.open(function(error) {
-  if (error) {
-    console.log('failed to open: ' + error);
-  } else {
-    console.log('open');
-    serialPort.on('data', function(data) {
-      scan(new Buffer(data).toString('ascii'))
-    });
-  }
-});
+//serialPort.open(function(error) {
+  //if (error) {
+    //console.log('failed to open: ' + error);
+  //} else {
+    //console.log('open');
+    //serialPort.on('data', function(data) {
+      //scan(new Buffer(data).toString('ascii'))
+    //});
+  //}
+//});
